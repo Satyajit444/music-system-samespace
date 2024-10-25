b63583e7b095e28a051de9a17072b87f920dcf00
satyajit444777 <satyajit.senapati@nettantra.net>
2024-10-25 21:38:19 +0530

    updates on music list fetch

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="flex flex-col justify-between h-full w-1/6 border py-[3%] px-[2%]">
+      {/* <img src="" alt="spotify_logo" /> */}
+      <h3>Spotify</h3>
 
-export default Sidebar
+      <p>settings</p>
+    </div>
+  );
+};
+
+export default Sidebar;
