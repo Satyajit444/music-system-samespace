e811b130c36d12fe7875914dbb386b3f2611abb7
satyajit444777 <satyajit.senapati@nettantra.net>
2024-10-26 17:34:57 +0530

    updated app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,100 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import Sidebar from "./components/shared/Sidebar";
 import SoundPlayer from "./components/SoundPlayer";
 import MusicList from "./components/musicTabs/MusicList";
 import axios from "axios";
+import { FaSearch } from "react-icons/fa";
 
 function App() {
   const [tab, setTab] = useState("for-you");
   const [songs, setSongs] = useState([]);
-  const [currentSong, setCurrentSong] = useState(null); // Track selected song
+  const [currentSong, setCurrentSong] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedAccent, setSelectedAccent] = useState("#000");
 
   useEffect(() => {
-    axios
-      .get("https://cms.samespace.com/items/songs")
-      .then((response) => setSongs(response.data.data))
-      .catch((error) => console.error("Error fetching songs:", error));
+    const fetchSongs = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://cms.samespace.com/items/songs"
+        );
+        setSongs(data?.data);
+      } catch (error) {
+        console.error("Error fetching songs:", error);
+      }
+    };
+    fetchSongs();
   }, []);
 
-  const topTracks = songs.filter((song) => song.top_track);
-  const forYouTracks = songs.filter((song) => !song.top_track);
+  const filteredSongs = useMemo(() => {
+    return songs
+      .filter((song) => (tab === "for-you" ? song : song.top_track))
+      .filter(
+        (song) =>
+          song.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          song.artist.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+  }, [songs, tab, searchTerm]);
 
+  const handleSongClick = (song) => {
+    setCurrentSong(song);
+    setSelectedAccent(song.accent || "#000");
+  };
   return (
-    <div className="h-[100vh] w-full flex">
+    <div
+      className="h-[100vh] w-full flex main-wrapper text-white"
+      style={{
+        background: `linear-gradient(135deg, ${selectedAccent}, ${selectedAccent}90 95%)`,
+        backdropFilter: "blur(10px)",
+        opacity: 0.8,
+      }}
+    >
       <Sidebar />
-
-      <div className="border border-red-400 w-full flex">
-        <div className="flex flex-col items-start w-1/2 p-[3%]">
+      <div className="w-full flex">
+        <div className="flex flex-col items-start w-1/3 py-[3%]">
           <div className="flex justify-start w-full gap-3">
             <button
-              onClick={() => setTab("for-you")}
+              onClick={() => {
+                setTab("for-you");
+                setSearchTerm("");
+              }}
               className={tab === "for-you" ? "font-bold" : ""}
             >
               For You
             </button>
             <button
-              onClick={() => setTab("top-tracks")}
+              onClick={() => {
+                setTab("top-tracks");
+                setSearchTerm("");
+              }}
               className={tab === "top-tracks" ? "font-bold" : ""}
             >
               Top Tracks
             </button>
           </div>
-          <div className="border border-red-600 w-full h-full">
-            {tab === "for-you" ? (
-              <MusicList songs={forYouTracks} onSelectSong={setCurrentSong} />
-            ) : (
-              <MusicList songs={topTracks} onSelectSong={setCurrentSong} />
-            )}
+          <div
+            className="py-2 px-4 mb-4 w-full rounded-md my-4 flex justify-between items-center"
+            style={{
+              background: `linear-gradient(135deg, ${selectedAccent}, ${selectedAccent})`,
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Search Song, Artist ..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="bg-transparent outline-none"
+            />
+            <FaSearch size={20} color="#777" />
+          </div>
+
+          <div className="w-full h-full overflow-auto scrollbar-hide animate-slideInFromLeft">
+            <MusicList songs={filteredSongs} onSelectSong={handleSongClick} />
           </div>
         </div>
 
-        <div className="w-full border border-green-900 p-[3%]">
+        <div className="w-full h-full p-[3%] flex items-center justify-center">
           <SoundPlayer song={currentSong} />
         </div>
       </div>
