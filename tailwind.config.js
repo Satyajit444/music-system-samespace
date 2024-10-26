7fec2f5a11e3a0191a23a6d9cd8dbb2105243c68
satyajit444777 <satyajit.senapati@nettantra.net>
2024-10-26 17:34:48 +0530

    added design effect on css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        slideInFromLeft: {
+          "0%": { opacity: "0", transform: "translateX(-100%)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        slideInFromLeft: "slideInFromLeft 0.5s ease forwards",
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".scrollbar-hide": {
+          "-ms-overflow-style": "none", // IE and Edge
+          "scrollbar-width": "none", // Firefox
+        },
+        ".scrollbar-hide::-webkit-scrollbar": {
+          display: "none", // Chrome, Safari, Opera
+        },
+      });
+    },
+  ],
+};
